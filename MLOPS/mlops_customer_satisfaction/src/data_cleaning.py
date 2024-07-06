d3ab36fdd3234f988c5b71f7594a3a73bdb47506
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-07-06 23:05:32 +0530

    updated data cleaning, build data statergy and preprocessing (pipeline)

---
+++ MLOPS/mlops_customer_satisfaction/src/data_cleaning.py
@@ -0,0 +1,90 @@
+import logging 
+from abc import ABC, abstractmethod
+from typing import Union
+
+import numpy as np 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+class DataStatergy(ABC):
+    """
+    Abstract class defining staergy for handling data
+    """
+    @abstractmethod
+    def handle_data(self, data: pd.DataFrame) -> Union [pd.DataFrame, pd.Series]:
+        pass
+
+
+class DataPreProcessStatergy(DataStatergy):
+    """
+    Statergy to preprocess data
+    """
+    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame :
+        """
+        PreProcess data
+        """
+        try: 
+            data = data.drop(
+                [
+                "order_approved_at",
+                "orderydelivered_carrier _date",
+                "order_delivered_ customer _date",
+                "order_estimated_delivery_date",
+                "order_purchase_timestamp",
+                ],
+                axis=1
+            )
+            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
+            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
+            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
+            data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
+            # write "No review" in review_comment_message column
+            data["review_comment_message"].fillna("No review", inplace=True)
+
+            #selecting the data which are of numeric value only 
+            data = data.select_dtypes(include=[np.number])            
+            cols_to_drop = ["customer_zip_code_prefix", "order_item_id"]
+            data = data.drop(cols_to_drop, axis=1)
+            return data
+        except Exception as e:
+            logging.error("Error in preprocessing data: {}".format(e))
+            raise e
+        
+class DataDivideStratergy(DataStatergy):
+    """
+    statergy to divide data into train and test data
+    """
+    def handle_data(self, data: pd.DataFrame) ->  Union [pd.DataFrame, pd.Series] :
+        """
+        divide data into train and test data
+        """
+        try:
+            X = data.drop(["review_score"], axis = 1)
+            y = data["review_score"]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            return X_train, X_test, y_train, y_test 
+        except Exception as e:
+            logging.error("Error in dividing the data: {}".format(e))
+            raise e
+        
+
+class DataClearning:
+    """
+    class for cleaning the data and which preprocess and divides data into train test
+    """
+    def __init__(self, data: pd.DataFrame, statergy: DataStatergy):
+        self.data = data
+        self.statergy = statergy
+    
+    def handle_data(self) -> Union [pd.DataFrame, pd.Series]:
+        """
+        Handle data
+        """
+        try:
+            return self.statergy.handle_data(self.data)
+        except Exception as e:
+            logging.error("Error in handling the data: {}".format(e))
+            raise e
+
+
+
--- MLOPS/mlops_customer_satisfaction/steps/clean_data.py
+++ MLOPS/mlops_customer_satisfaction/steps/clean_data.py
@@ -2,9 +2,37 @@ import logging
 import pandas as pd
 from zenml import step
 
-
+from src.data_cleaning import DataClearning, DataDivideStratergy, DataPreProcessStatergy
+from typing_extensions import Annotated
+from typing import Tuple
 
 
 @step
-def clean_df(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+def clean_df(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_test"]
+]:
+    """
+    Cleans data and divides it into train and test
+    Args:
+        df: Raw data
+    Returns:
+        X_train: training data
+        X_test: test data
+        y_train: training data
+        y_test: test data
+    """
+    try:
+        process_statergy = DataPreProcessStatergy()
+        data_cleaning = DataClearning(df, process_statergy)
+        processed_data= data_cleaning.handle_data()
+
+        divide_statergy = DataDivideStratergy()
+        data_cleaning = DataClearning(processed_data, divide_statergy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data Cleaning Compleated")
+    except Exception as e:
+        logging.error("Error in cleaning the data: {}".format(e))
+        raise e
