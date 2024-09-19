d9168857992d4633c056bb4e8f5f283dd0982ccd
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-09-19 10:39:33 +0530

    ml flow eg

---
+++ MLOPS/mlop_2/eg.py
@@ -0,0 +1,83 @@
+# importing the libraries
+import os
+import warnings
+import sys
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split    
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+import logging
+
+# logging
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def evel_metrices(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae,r2
+
+
+
+if __name__=="__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    #reading the csv file
+    csv_url = (
+    "<enter csv url here>"
+    )
+    try:
+        data = pd.read_csv(csv_url, sep="")
+    except Exception as e:
+        logger.exception(
+            "Unable to download training and test csv, check your internet connection : %s ",e
+        )
+
+    #splt the data into training and test split
+    train, test = train_test_split(data)
+
+    # the prediction column is "quality" which is a scaler 
+    train_x =train.drop(["quality"], axis = 1)
+    test_x = test.drop(["quality"], axis = 1)
+    train_y = train(["quality"])
+    test_y=test(["quality"])
+
+    alpha = float(sys.argv[1]) if len(sys.argv)>1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv)>2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state = 42)
+        lr.fit(train_x, train_y)
+
+        predictino_quantity = lr.predcit(test_x)
+
+    (rmse, mae,r2) = evel_metrices(test_y, predictino_quantity)
+
+
+    mlflow.log_param("alpha: :", alpha)
+    mlflow.log_param("l1_ration :", l1_ratio)
+    mlflow.log_param("rmse :", rmse)
+    mlflow.log_param("r2 :", r2)
+    mlflow.log_param("mae :",mae)
+
+    predictions  = lr.predict(train_x)
+    signature = infer_signature(train_x, predictions)
+
+    tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+    #model registery does not work with file store
+    if tracking_url_type_store != "file":
+        #tracking in uri
+        mlflow.sklearn.log_model(
+            lr, "model", registered_model_name="ElasticcnetWithModel", signature=signature
+        )
+    else:
+        #tracking in local
+        mlflow.sklearn.log_model(lr, "model", signature=signature)
