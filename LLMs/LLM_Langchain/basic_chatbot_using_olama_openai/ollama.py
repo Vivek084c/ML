b1a537b927674d64101b9d9a310fcecc5d004243
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-10-16 09:44:52 +0530

    using ollam for chatbot

---
+++ LLMs/LLM_Langchain/basic_chatbot_using_olama_openai/ollama.py
@@ -0,0 +1,32 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import ollama
+
+from dotenv import load_dotenv
+import os
+import streamlit as st
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("openai_api_key")
+os.environ["LANGCHAIN_TRACKING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("langchain_api_key")
+
+# promt templated
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","you are a helpfull assistance, please response to the user queries"),
+        ("user","Question : {question}")
+    ]
+)
+
+#srtreamlit framework
+st.title("Chatbot")
+input_text = st.text_input("Search the topic you want")
+
+#calling openai llm
+llm=ollama(model="llama3.2")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
