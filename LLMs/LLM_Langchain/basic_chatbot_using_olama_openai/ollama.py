f998f9f42a9cd1af5201f50df96d82d55d682910
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2024-10-16 11:49:13 +0530

    update

--- LLMs/LLM_Langchain/basic_chatbot_using_olama_openai/ollama.py
+++ LLMs/LLM_Langchain/basic_chatbot_using_olama_openai/ollama.py
@@ -1,13 +1,14 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
+
 
 from dotenv import load_dotenv
 import os
 import streamlit as st
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("openai_api_key")
+
 os.environ["LANGCHAIN_TRACKING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("langchain_api_key")
 
@@ -24,7 +25,7 @@ st.title("Chatbot")
 input_text = st.text_input("Search the topic you want")
 
 #calling openai llm
-llm=ollama(model="llama3.2")
+llm = Ollama(model="llama3.2")
 output_parser = StrOutputParser()
 chain = prompt|llm|output_parser
 
---
+++ LLMs/LLM_Langchain/basic_chatbot_using_olama_openai/temp.py
@@ -0,0 +1,4 @@
+from langchain_community.llms import ollama
+# Example of listing available models (refer to ollama's API docs for the exact function if available)
+available_models = ollama.
+print(available_models)
