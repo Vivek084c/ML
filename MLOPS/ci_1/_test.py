0df719b9dd2b443f1da908030f9d43b1dda0e8db
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-02-04 22:29:20 +0530

    ci file test 2

--- MLOPS/ci_1/_test.py
+++ MLOPS/ci_1/_test.py
@@ -1,33 +1,33 @@
 import pytest
 
-#function to test square
+# Function to test square
 def square(n):
     return n**2
 
-#function to test cube
+# Function to test cube
 def cube(n):
     return n**3
 
-#function to test the fifth power
+# Function to test the fifth power
 def fifth(n):
     return n**5
 
-#testing the square function 
+# Testing the square function 
 def test_square():
-    assert square(2)==4, "Test failed: the square of 2 is 4"
-    assert square(3)==9, "Test failed: the square of 3 is 9"
+    assert square(2) == 4, "Test failed: the square of 2 is 4"
+    assert square(3) == 9, "Test failed: the square of 3 is 9"
 
-#testing the cube function 
+# Testing the cube function 
 def test_cube():
-    assert square(2)==8, "Test failed: the square of 2 is 8"
-    assert square(3)==27, "Test failed: the square of 3 is 27"
+    assert cube(2) == 8, "Test failed: the cube of 2 is 8"
+    assert cube(3) == 27, "Test failed: the cube of 3 is 27"
 
-#testing the fifth function 
+# Testing the fifth function 
 def test_fifth():
-    assert square(2)==32, "Test failed: the fifth of 2 is 32"
-    assert square(3)==253, "Test failed: the fifth of 3 is 243"
+    assert fifth(2) == 32, "Test failed: the fifth of 2 is 32"
+    assert fifth(3) == 243, "Test failed: the fifth of 3 is 243"
 
-#test for invalid input
+# Test for invalid input
 def test_invalid_input():
     with pytest.raises(TypeError):
         square("string")
