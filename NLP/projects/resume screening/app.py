6c0df0191ed75c9bce3fd08f6f7d016f3517495f
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-02-15 23:54:56 +0530

    done with resume screening project

--- NLP/projects/resume screening/app.py
+++ NLP/projects/resume screening/app.py
@@ -15,12 +15,63 @@ clf = pickle.load(open('clf.pkl', 'rb'))
 tfidf = pickle.load(open('tfidf.pkl', 'rb'))
 
 
+def cleanResume(txt):
+    cleanText = re.sub('http\S+\s', ' ', txt)
+    cleanText = re.sub('RT|cc', ' ', cleanText)
+    cleanText = re.sub('#\S+\s', ' ', cleanText)
+    cleanText = re.sub('@\S+', '  ', cleanText)  
+    cleanText = re.sub('[%s]' % re.escape("""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""), ' ', cleanText)
+    cleanText = re.sub(r'[^\x00-\x7f]', ' ', cleanText) 
+    cleanText = re.sub('\s+', ' ', cleanText)
+    return cleanText
+
+mpp ={6: 'Data Science',
+ 12: 'HR',
+ 0: 'Advocate',
+ 1: 'Arts',
+ 24: 'Web Designing',
+ 16: 'Mechanical Engineer',
+ 22: 'Sales',
+ 14: 'Health and fitness',
+ 5: 'Civil Engineer',
+ 15: 'Java Developer',
+ 4: 'Business Analyst',
+ 21: 'SAP Developer',
+ 2: 'Automation Testing',
+ 11: 'Electrical Engineering',
+ 18: 'Operations Manager',
+ 20: 'Python Developer',
+ 8: 'DevOps Engineer',
+ 17: 'Network Security Engineer',
+ 19: 'PMO',
+ 7: 'Database',
+ 13: 'Hadoop',
+ 10: 'ETL Developer',
+ 9: 'DotNet Developer',
+ 3: 'Blockchain',
+ 23: 'Testing'}
+
 #creating a streamlit app
 def main():
     st.title("resume screener app") 
     
     #asking user to upload the resume
-    st.file_uploader("Upload your reseum", type=['pdf', 'txt'])
+    uploaded_file = st.file_uploader("Upload your reseum", type=['pdf', 'txt'])
+
+    #checking if the file is not empty
+    if uploaded_file is not None:
+        try:
+            resume_byte = uploaded_file.read()
+            resume_text = resume_byte.decode('utf-8')
+        except UnicodeDecodeError:
+            #trying latin decoder
+            resume_text  = resume_byte.decode('latin-1')
+    else:
+        return 
+    clean_resume = cleanResume(resume_text)
+    clean_resume = tfidf.transform([clean_resume])
+    prediction_id = clf.predict(clean_resume)[0]
+    st.write("job profile : ",mpp[prediction_id])
 
 
 #python main 
