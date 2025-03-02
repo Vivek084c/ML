4c7fc02b38c46d49ab81b70d70f6e5dd20de1a18
Vivek Choudhry <126272453+Vivek084c@users.noreply.github.com>
2025-03-03 01:10:17 +0530

    done with ner

--- NLP/projects/Named_Entity_Recognition/app.py
+++ NLP/projects/Named_Entity_Recognition/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import spacy
 from spacy import displacy
+nlp = spacy.load('en_core_web_sm')
+
 
 app = Flask(__name__)
 
@@ -12,10 +14,25 @@ def home():
 
 @app.route('/entity', methods=['POST', 'GET'])
 def entity():
+    html = None
+    text = None
+
     if request.method=='POST':
         #if the requested method is post the perform processing
-        file = request.files['file']
+        file = request.files.get('file')
 
         #checking if file content is present
         if file:
-            readable_file  = file.read().deocde('utf-8', errors = 'ignore')
+            text  = file.read().decode('utf-8', errors='ignore')
+            docs = nlp(text)
+
+            #rendering to html 
+            html = displacy.render(docs, style='ent')
+
+            #returing the data to render on the html page
+    return render_template('index.html', html=html, text = text)
+        
+#creating a main point
+if __name__ == "__main__":
+    app.run(debug=True)
+
